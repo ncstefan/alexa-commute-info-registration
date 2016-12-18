975c1cfdd22cb901c48a119e45dbce1375f5bb5f
Nick <ns@netmail.com>
2016-12-18 10:13:25 -0500

    initial commit

---
+++ rts_index.js
@@ -0,0 +1,11 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { appTitle: 'Commute Info Registration'});
+
+console.log("index route                                      " );
+
+};
